7ad94a7188e5d47591f5d2e03047372284f9a6ca
csrap <caugusto3110@gmail.com>
2020-11-10 19:53:23 -0300

    Barajas con Funciones

---
+++ barajas.py
@@ -0,0 +1,14 @@
+palos =  ['o', 'c', 'e', 'b']
+numeros = [ 'A', '2', '3', '4', '5','6','7', 'S', 'C','R']
+
+baraja  = []
+
+def crearBaraja(): 
+  
+   for palo in palos:
+      for numero in numeros:
+        baraja.append (numero+palo)
+        return baraja 
+    
+crearBaraja() 
+print(araja) 
